b970d8f68d2153635fef9b3b3d977641138056f4
lonich <lanilor53@gmail.com>
2020-05-01 15:08:25 +0300

    Change movement mechanics, add sky collision

--- Player.gd
+++ Player.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
-var speed = 0.5
-var speedMod = 0.05
-var maxSpeed = 1
-var minSpeed = 0
-
+var speed = 5
+var speedMod = 0.5
+var maxSpeed = 50
+var minSpeed = 1
+var oldpos = Vector2.ZERO
+onready var sky = get_parent().get_node("bgstub/Sky")
 
 func _process(delta):
 	# TODO: cooldown
@@ -16,17 +17,20 @@ func _process(delta):
 		slowdown(speedMod)
 
 func _physics_process(delta):
-	var mousePos = get_global_mouse_position()
+	var mousePos = get_local_mouse_position()
 	# Position must be slightly to the left
 	#TODO: collision
-	position = lerp(position,mousePos-Vector2(140,0),speed/10)
-	
+	#lerp(position,mousePos-Vector2(140,0),speed/10)
+	if(pow(mousePos.x,2)+pow(mousePos.y,2)>10):
+		move_and_slide(mousePos.normalized()*speed*100)
+
+
 func speedup(mod):
-	if speed+mod < maxSpeed:
+	if speed+mod <= maxSpeed:
 		speed += mod
 	print(speed)
 
 func slowdown(mod):
-	if speed-mod > minSpeed:
+	if speed-mod >= minSpeed:
 		speed -= mod
 	print(speed)
